90b50457a5e061ff49ba4185e564b60c758ae077
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-02-03 23:13:01 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { NavbarSimple } from './components/navBar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/DashBoard';
+import { EcommerceCard } from './pages/Products';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route element={<NavbarSimple />}>
+      <Router>
+        <NavbarSimple />
+        <Routes>
           <Route path="/" element={<Home />} />
-        </Route>
-      </Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/products" element={<EcommerceCard />} />
+          {/* <Route path="/register" element={<Register />} /> */}
+        </Routes>
+      </Router>
     </>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import './input.css';
 import './output.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-tailwind/react';
-import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './middleware/authContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider>
+    <ThemeProvider>
+      <AuthProvider>
         <App />
-      </ThemeProvider>
-    </BrowserRouter>
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
