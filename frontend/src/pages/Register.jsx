783e5dd114181a3ca95c8471262d3f2a29f98ad3
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-02-03 23:12:03 +0530

    register UI

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+import {
+  Card,
+  Input,
+  Checkbox,
+  Button,
+  Typography,
+} from '@material-tailwind/react';
+import { Link, useNavigate } from 'react-router-dom';
+
+export default function Register() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post(
+        'http://localhost:8070/api/user/create',
+        formData
+      );
+
+      console.log('Registration successful:', response.data);
+      window.alert('Registration success');
+      navigate('/dashboard');
+
+      // Handle success, e.g., redirect to login page
+    } catch (error) {
+      console.error('Registration failed:', error.response.data);
+      window.alert('Wada na ');
+      // Handle error, e.g., show an error message
+    }
+  };
+
+  return (
+    <div className="Register">
+      <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign Up
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Nice to meet you! Enter your details to register.
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Name
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="Enter your name"
+              name="name"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your Email
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              name="email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Password
+            </Typography>
+            <Input
+              type="password"
+              size="lg"
+              name="password"
+              placeholder="********"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+            />
+          </div>
+          <Checkbox
+            label={
+              <Typography
+                variant="small"
+                color="gray"
+                className="flex items-center font-normal"
+              >
+                I agree to the
+                <Link
+                  to="#"
+                  className="font-medium transition-colors hover:text-gray-900"
+                >
+                  &nbsp;Terms and Conditions
+                </Link>
+              </Typography>
+            }
+            containerProps={{ className: '-ml-2.5' }}
+          />
+          <Button className="mt-6" fullWidth onClick={handleRegister}>
+            Sign up
+          </Button>
+          <Typography color="gray" className="mt-4 text-center font-normal">
+            Already have an account?{' '}
+            <Link to="/login" className="font-medium text-gray-900">
+              Sign In
+            </Link>
+          </Typography>
+        </form>
+      </Card>
+    </div>
+  );
+}
