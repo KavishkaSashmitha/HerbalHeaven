86c5544ead1de8f90ab6ae2f9a853fb574a7603d
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-02-08 17:39:30 +0530

    slider import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './input.css';
 import './output.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-tailwind/react';
