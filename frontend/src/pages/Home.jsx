c305e26d197e6efcd99222a233c34702ae6d15a0
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-02-08 16:26:33 +0530

    slider

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import ImageSlider from '../components/Slider';
 
 function Home() {
+  const images = [
+    '/slider/slider_1.png',
+    '/slider/slider_2.png',
+    '/slider/slider_3.png',
+
+    // Add more image URLs as needed
+  ];
+
   return (
     <div>
-      <img
-        src="/home.gif
-    "
-        alt=""
-      />
+      <ImageSlider images={images} />
     </div>
   );
 }
