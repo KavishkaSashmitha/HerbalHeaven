1bc0bf65256097d7732995e4aa34e93c6b29f74c
Pasindu Rasanjana <153684733+Pasindu457825@users.noreply.github.com>
2024-03-18 09:13:03 +0530

    form error done

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -204,9 +204,6 @@ export default class Posts extends Component {
                     Action
                   </p>
                 </th>
-                <th class="p-4 border-y border-blue-gray-100 bg-blue-gray-50/50">
-                  <p class="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70"></p>
-                </th>
               </tr>
 
               <tbody>
