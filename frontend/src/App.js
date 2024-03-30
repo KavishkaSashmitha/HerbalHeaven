2804a05b43513fd1b156a819e1e1ec06dae83c68
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-03-30 21:52:29 +0530

    some changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,30 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import { SidebarWithBurgerMenu } from "./components/navBar";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Dashboard from "./pages/DashBoard";
-import { EcommerceCard } from "./pages/Products";
-import Cart from "./pages/cart";
-import CreatPost from "./components/CreateEmployee";
-import EditPost from "./components/EditEmployee";
-import Posts from "./components/Employee";
-import PostDetails from "./components/EmployeeDetails";
-import "react-toastify/dist/ReactToastify.css";
-import Payment from "./pages-Kumesh/Payment";
-import CreateUser from "./pages_kavindu/CreateUser";
-import UpdateUser from "./pages_kavindu/UpdateUser";
-import User from "./pages_kavindu/User";
-import AdminDashboard from "./pages/adminDashboard";
-import SalaryReport from "./components/SalaryReport";
-import "./index.css";
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import { SidebarWithBurgerMenu } from './components/navBar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/DashBoard';
+import { EcommerceCard } from './pages/Products';
+import Cart from './pages/cart';
+import CreatPost from './components/CreateEmployee';
+import EditPost from './components/EditEmployee';
+import Posts from './components/Employee';
+import PostDetails from './components/EmployeeDetails';
+import 'react-toastify/dist/ReactToastify.css';
+import Payment from './pages-Kumesh/Payment';
+import CreateUser from './pages_kavindu/CreateUser';
+import UpdateUser from './pages_kavindu/UpdateUser';
+import User from './pages_kavindu/User';
+import AdminDashboard from './pages/adminDashboard';
+import SalaryReport from './components/SalaryReport';
+import './index.css';
+
 
 function App() {
   return (
     <>
       <Routes>
+        
         {/* Kavishka */}
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -32,14 +34,11 @@ function App() {
         <Route path="/products" element={<EcommerceCard />} />
         <Route path="/user/cart" element={<Cart />} />
 
-
-
         <Route path="/emp/add" element={<CreatPost />} />
         <Route path="/emp/edit/:id" element={<EditPost />} />
         <Route path="/emp/:id" element={<PostDetails />} />
         <Route path="/emp" element={<Posts />} />
 
-        
         <Route path="/user/payment" element={<Payment />} />
 
         <Route path="/sup/addsup" element={<CreateUser />} />
