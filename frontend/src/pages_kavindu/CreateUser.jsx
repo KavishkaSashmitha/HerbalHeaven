5bff205f0c830b541db8a3d97d8d2fe1a24fe766
Kavindu Galgamuwa <125373634+kavindunilusha@users.noreply.github.com>
2024-04-03 16:18:06 +0530

    changes

--- frontend/src/pages_kavindu/CreateUser.jsx
+++ frontend/src/pages_kavindu/CreateUser.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Footer } from "../components/Footer";
 import { SidebarWithBurgerMenu } from "../components/navBar";
 import { useNavigate } from "react-router-dom";
 import {
@@ -107,109 +108,112 @@ function CreateUser() {
   };
 
   return (
-    <div className="bg-image01">
-      <SidebarWithBurgerMenu />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "",
-        }}
-      >
-        <Card className="w-96">
-          <CardHeader
-            variant="gradient"
-            color="white"
-            className="mb-4 grid h-28 place-items-center"
-          >
-            <Typography variant="h3" color="black">
-              Add New Suppliers
-            </Typography>
-          </CardHeader>
-          <CardBody className="flex flex-col gap-4">
-            <Input
-              label="Name"
-              size="lg"
-              value={name}
-              onChange={(e) => {
-                const value = e.target.value.replace(/[^a-zA-Z\s]/g, "");
-                setName(value);
-              }}
-              error={errors.name}
-            />
-            <Input
-              label="Email"
-              size="lg"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              error={errors.email}
-            />
-            <Input
-              label="Age"
-              size="lg"
-              type="number"
-              value={age}
-              onChange={(e) => {
-                let value = e.target.value.replace(/\D/, ""); // Remove non-numeric characters
-                value = value === "" ? "" : Math.min(Math.max(value, 1), 100);
-                setAge(value);
-              }}
-              error={errors.age}
-            />
-            <Input
-              label="Raw Material"
-              size="lg"
-              value={rawMaterial}
-              onChange={(e) => {
-                const value = e.target.value.replace(/[^A-Za-z]/gi, "");
-                setRawMaterial(value);
-              }}
-              error={errors.rawMaterial}
-            />
-            <Input
-              label="Country"
-              size="lg"
-              value={country}
-              onChange={(e) => {
-                const value = e.target.value.replace(/[^a-zA-Z\s]/g, "");
-                setCountry(value);
-              }}
-              error={errors.country}
-            />
-
-            <Input
-              label="Mobile"
-              size="lg"
-              value={mobile}
-              onChange={(e) => {
-                if (/^\d{0,10}$/.test(e.target.value)) {
-                  setMobile(e.target.value);
-                }
-              }}
-              error={errors.mobile}
-            />
-            <Input
-              label="Address"
-              size="lg"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              error={errors.address}
-            />
-          </CardBody>
-          <CardFooter className="pt-0">
-            <button
-              className="m-2 relative select-none rounded-lg bg-orange-500 py-3.5 px-14 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              type="submit"
-              onClick={handleSubmit}
+    <>
+      <div className="bg-image01">
+        <SidebarWithBurgerMenu />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "",
+          }}
+        >
+          <Card className="w-96">
+            <CardHeader
+              variant="gradient"
+              color="white"
+              className="mb-4 grid h-28 place-items-center"
             >
-              &nbsp;Add New Supplier
-            </button>
-          </CardFooter>
-        </Card>
+              <Typography variant="h3" color="black">
+                Add New Suppliers
+              </Typography>
+            </CardHeader>
+            <CardBody className="flex flex-col gap-4">
+              <Input
+                label="Name"
+                size="lg"
+                value={name}
+                onChange={(e) => {
+                  const value = e.target.value.replace(/[^a-zA-Z\s]/g, "");
+                  setName(value);
+                }}
+                error={errors.name}
+              />
+              <Input
+                label="Email"
+                size="lg"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                error={errors.email}
+              />
+              <Input
+                label="Age"
+                size="lg"
+                type="number"
+                value={age}
+                onChange={(e) => {
+                  let value = e.target.value.replace(/\D/, ""); // Remove non-numeric characters
+                  value = value === "" ? "" : Math.min(Math.max(value, 1), 100);
+                  setAge(value);
+                }}
+                error={errors.age}
+              />
+              <Input
+                label="Raw Material"
+                size="lg"
+                value={rawMaterial}
+                onChange={(e) => {
+                  const value = e.target.value.replace(/[^A-Za-z]/gi, "");
+                  setRawMaterial(value);
+                }}
+                error={errors.rawMaterial}
+              />
+              <Input
+                label="Country"
+                size="lg"
+                value={country}
+                onChange={(e) => {
+                  const value = e.target.value.replace(/[^a-zA-Z\s]/g, "");
+                  setCountry(value);
+                }}
+                error={errors.country}
+              />
+
+              <Input
+                label="Mobile"
+                size="lg"
+                value={mobile}
+                onChange={(e) => {
+                  if (/^\d{0,10}$/.test(e.target.value)) {
+                    setMobile(e.target.value);
+                  }
+                }}
+                error={errors.mobile}
+              />
+              <Input
+                label="Address"
+                size="lg"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                error={errors.address}
+              />
+            </CardBody>
+            <CardFooter className="pt-0">
+              <button
+                className="m-2 relative select-none rounded-lg bg-orange-500 py-3.5 px-14 text-center align-middle font-sans text-sm font-bold uppercase text-white shadow-md shadow-gray-900/10 transition-all hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                type="submit"
+                onClick={handleSubmit}
+              >
+                &nbsp;Add New Supplier
+              </button>
+            </CardFooter>
+          </Card>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/pages_kavindu/User.jsx
+++ frontend/src/pages_kavindu/User.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Card, Typography, Button } from "@material-tailwind/react";
-import { Link } from "react-router-dom";
 import axios from "axios";
-import { Input } from "@material-tailwind/react";
+import { Link } from "react-router-dom";
+import { Input, Card, Typography, Button } from "@material-tailwind/react";
+import { Footer } from "../components/Footer";
 import { SidebarWithBurgerMenu } from "../components/navBar";
 
 const User = () => {
@@ -43,155 +43,158 @@ const User = () => {
   });
 
   return (
-    <div className="bg-image01">
-      <SidebarWithBurgerMenu />
-      <div class="relative mx-4 mt-4 overflow-hidden text-gray-700  ">
-        <div class="flex items-center justify-between gap-8 mb-8">
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              height: "",
-            }}
-          >
-            <Card className="w-full max-w-4xl p-5 border-blue-gray-100 bg-blue-gray-50/50">
-              <h1>List of Suppliers</h1>
-              <Input
-                type="text"
-                placeholder="Search..."
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="mb-4 border-blue-gray-100 bg-blue-gray-50/50"
-              />
+    <>
+      <div className="bg-image01">
+        <SidebarWithBurgerMenu />
+        <div className="relative mx-4 mt-4 overflow-hidden text-gray-700">
+          <div className="flex items-center justify-between gap-8 mb-8">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "",
+              }}
+            >
+              <Card className="w-full max-w-4xl p-5 border-blue-gray-100 bg-blue-gray-50/50">
+                <h1>List of Suppliers</h1>
+                <Input
+                  type="text"
+                  placeholder="Search..."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="mb-4 border-blue-gray-100 bg-blue-gray-50/50"
+                />
 
-              <table className="w-full min-w-max table-auto text-left border-blue-gray-100 bg-blue-gray-50/50">
-                <thead>
-                  <tr>
-                    {[
-                      "Name",
-                      "Email",
-                      "Age",
-                      "Raw Material",
-                      "Country",
-                      "Mobile",
-                      "Address",
-                      "Action",
-                    ].map((head, index) => (
-                      <th
-                        key={index}
-                        className="p-4 border-b border-blue-gray-100 bg-blue-gray-50/50"
-                      >
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal leading-none opacity-70 border-blue-gray-100 bg-blue-gray-50/50"
+                <table className="w-full min-w-max table-auto text-left border-blue-gray-100 bg-blue-gray-50/50">
+                  <thead>
+                    <tr>
+                      {[
+                        "Name",
+                        "Email",
+                        "Age",
+                        "Raw Material",
+                        "Country",
+                        "Mobile",
+                        "Address",
+                        "Action",
+                      ].map((head, index) => (
+                        <th
+                          key={index}
+                          className="p-4 border-b border-blue-gray-100 bg-blue-gray-50/50"
                         >
-                          {head}
-                        </Typography>
-                      </th>
-                    ))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {filteredUsers.map((user) => (
-                    <tr
-                      key={user._id}
-                      className=" border-blue-gray-100 bg-blue-gray-50/50"
-                    >
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {user.name}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          className="font-normal border-blue-gray-100 bg-blue-gray-50/50"
-                        >
-                          {user.email}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {user.age}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {user.rawMaterial}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {user.country}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {user.mobile}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Typography
-                          variant="small"
-                          color="blue-gray"
-                          className="font-normal"
-                        >
-                          {user.address}
-                        </Typography>
-                      </td>
-                      <td className="p-4">
-                        <Link
-                          to={`/sup/update/${user._id}`}
-                          className="btn btn-warning"
-                        >
-                          <Button color="Black">Update</Button>
-                        </Link>
-                        <Button
-                          color="red"
-                          onClick={() => handleDelete(user._id)}
-                        >
-                          Delete
-                        </Button>
-                      </td>
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal leading-none opacity-70 border-blue-gray-100 bg-blue-gray-50/50"
+                          >
+                            {head}
+                          </Typography>
+                        </th>
+                      ))}
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-              <Link to="/sup/addsup">
-                <Button
-                  color="amber"
-                  className="py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                >
-                  Add New Supplier
-                </Button>
-              </Link>
-            </Card>
+                  </thead>
+                  <tbody>
+                    {filteredUsers.map((user) => (
+                      <tr
+                        key={user._id}
+                        className=" border-blue-gray-100 bg-blue-gray-50/50"
+                      >
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user.name}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            className="font-normal border-blue-gray-100 bg-blue-gray-50/50"
+                          >
+                            {user.email}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user.age}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user.rawMaterial}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user.country}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user.mobile}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Typography
+                            variant="small"
+                            color="blue-gray"
+                            className="font-normal"
+                          >
+                            {user.address}
+                          </Typography>
+                        </td>
+                        <td className="p-4">
+                          <Link
+                            to={`/sup/update/${user._id}`}
+                            className="btn btn-warning"
+                          >
+                            <Button color="Black">Update</Button>
+                          </Link>
+                          <Button
+                            color="red"
+                            onClick={() => handleDelete(user._id)}
+                          >
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <Link to="/sup/addsup">
+                  <Button
+                    color="amber"
+                    className="py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  >
+                    Add New Supplier
+                  </Button>
+                </Link>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
