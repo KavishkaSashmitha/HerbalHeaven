e3c9bd85433d634662476c370d209dc1151b3eb9
Pasindu Rasanjana <153684733+Pasindu457825@users.noreply.github.com>
2024-04-10 00:42:27 +0530

    hrii

--- frontend/src/pages_Pasindu/CreateEmployee.js
+++ frontend/src/pages_Pasindu/CreateEmployee.js
@@ -22,7 +22,7 @@ export default class CreatPost extends Component {
       address: "",
       isAdmin: "",
       age: "",
-      // image: "",
+      image: "",
       confirmation: false,
       errors: {},
 
