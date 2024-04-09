beb9b1023aa3ead63fac40b07bb790662d474e62
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-04-09 15:05:16 +0530

    sidebar changed mf

--- frontend/src/components/navBar.jsx
+++ frontend/src/components/navBar.jsx
@@ -24,6 +24,8 @@ import {
   ChevronRightIcon,
   ChevronDownIcon,
   PresentationChartBarIcon,
+  TruckIcon,
+  CubeIcon,
 } from '@heroicons/react/24/solid';
 import {
   MagnifyingGlassIcon,
@@ -105,7 +107,7 @@ export function SidebarWithBurgerMenu({}) {
                     className="border-b-0 p-3"
                   >
                     <ListItemPrefix>
-                      <ShoppingBagIcon className="h-5 w-5" />
+                      <TruckIcon className="h-5 w-5" />
                     </ListItemPrefix>
                     <Typography
                       color="blue-gray"
@@ -125,20 +127,7 @@ export function SidebarWithBurgerMenu({}) {
                     className="border-b-0 p-3"
                   >
                     <ListItemPrefix>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
-                        />
-                      </svg>
+                      <CubeIcon className="h-5 w-5" />
                     </ListItemPrefix>
                     <Typography
                       color="blue-gray"
