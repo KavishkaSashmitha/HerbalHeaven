eda449cf5278fc88cced4a69f2e3deee2900ed92
Ridmi <ridmihansini97@gmail.com>
2024-04-10 12:25:56 +0530

    get selected items from cart to payment page

--- frontend/src/pages-Kumesh/User/Payment.jsx
+++ frontend/src/pages-Kumesh/User/Payment.jsx
@@ -21,33 +21,38 @@ function Payment() {
   const [cart, setCart] = useState([]);
   const { isLoggedIn, token } = useAuth();
 
-  useEffect(() => {
-    const fetchCartItems = async () => {
-      try {
-        if (isLoggedIn) {
-          const response = await axios.get(
-            "http://localhost:8070/api/user/cart",
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          );
-          // Remove duplicate items from the cart
-          const uniqueCartItems = Array.from(
-            new Set(response.data.map((item) => item.name))
-          ).map((name) => {
-            return response.data.find((item) => item.name === name);
-          });
-          setCart(response.data);
-        }
-      } catch (error) {
-        console.error("Error fetching cart items:", error);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchCartItems = async () => {
+  //     try {
+  //       if (isLoggedIn) {
+  //         const response = await axios.get(
+  //           "http://localhost:8070/api/user/cart",
+  //           {
+  //             headers: {
+  //               Authorization: `Bearer ${token}`,
+  //             },
+  //           }
+  //         );
+  //         // Remove duplicate items from the cart
+  //         const uniqueCartItems = Array.from(
+  //           new Set(response.data.map((item) => item.name))
+  //         ).map((name) => {
+  //           return response.data.find((item) => item.name === name);
+  //         });
+  //         setCart(response.data);
+  //       }
+  //     } catch (error) {
+  //       console.error("Error fetching cart items:", error);
+  //     }
+  //   };
 
-    fetchCartItems();
-  }, [isLoggedIn, token]);
+  //   fetchCartItems();
+  // }, [isLoggedIn, token]);
+
+  console.log("cart", cart);
+  useEffect(() => {
+    setCart(location.state.selectedCartItems);
+  }, [location.state.selectedCartItems]);
 
   // Define the steps with their corresponding route paths
   const steps = [
