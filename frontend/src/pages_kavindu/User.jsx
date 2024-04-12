3f40e700399d602c545e1d8c7cbe9993ba578ba9
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-04-12 10:24:04 +0530

    updated kavindu part
    Co-Authored-By: Pasindu Rasanjana <153684733+Pasindu457825@users.noreply.github.com>

--- frontend/src/pages_kavindu/User.jsx
+++ frontend/src/pages_kavindu/User.jsx
@@ -146,14 +146,6 @@ const User = () => {
                     </tbody>
                   </table>
                 </div>
-                <Link to="/sup/addsup">
-                  <Button
-                    color="amber"
-                    className="py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-black shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-                  >
-                    Add New Supplier
-                  </Button>
-                </Link>
               </Card>
             </div>
           </div>
