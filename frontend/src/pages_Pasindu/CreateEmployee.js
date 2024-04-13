4db817a57a114691dd9a03ca643608959e44060d
Pasindu Rasanjana <153684733+Pasindu457825@users.noreply.github.com>
2024-04-13 10:20:12 +0530

    comment removed
    Co-Authored-By: Kavishka Sashmitha <132291481+KavishkaSashmitha@users.noreply.github.com>

--- frontend/src/pages_Pasindu/CreateEmployee.js
+++ frontend/src/pages_Pasindu/CreateEmployee.js
@@ -196,13 +196,6 @@ export default function CreatePost() {
     }));
   };
 
-  // const handleIsAdminChange = (e) => {
-  //   setState((cs) => ({
-  //     ...cs,
-  //     isAdmin: e.target.value,
-  //   }));
-  // };
-
   const onSubmit = (e) => {
     e.preventDefault();
 
@@ -667,36 +660,7 @@ export default function CreatePost() {
                                 )}
                                 <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all before:content-none after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all after:content-none peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"></label>
                               </div>
-
-                              {/* Image preview
-                              {state.image && (
-                                <div className="mt-2">
-                                  <img
-                                    src={state.image}
-                                    alt="Preview"
-                                    className="w-full h-auto rounded-[7px]"
-                                  />
-                                </div>
-                              )} */}
                             </div>
-
-                            {/* <div>
-                              <p class="block mt-2 mb-1 font-sans text-x1 antialiased font-medium leading-normal text-blue-gray-900">
-                                <label>Admin Status</label>
-                              </p>
-                              <div class="relative h-10 w-full min-w-[200px]">
-                                <select
-                                  value={state?.isAdmin}
-                                  name="isAdmin"
-                                  onChange={handleIsAdminChange}
-                                  class="peer bg-white h-full w-full rounded-[7px] border border-blue-gray-200 border-t-transparent !border-t-blue-gray-200 bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-gray-900 focus:border-t-transparent focus:!border-t-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                                >
-                                  <option value="">Select Admin Status</option>
-                                  <option value="true">True</option>
-                                  <option value="false">False</option>
-                                </select>
-                              </div>
-                            </div> */}
                           </form>
                         </div>
                       </div>
