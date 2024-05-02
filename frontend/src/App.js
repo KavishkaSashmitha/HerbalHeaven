106857d9ce12d7902a9f177ca69d4b2e9d50f29e
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-05-02 17:06:24 +0530

    loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -111,7 +111,7 @@ import AdminDashboard from "./pages/adminDashboard";
 import DeretOrders from "./pages-Kumesh/Admin/Income/DeretOrders";
 import MaterialCost from "./pages-Kumesh/Admin/Expens/MaterialCost";
 import EmpSalary from "./pages-Kumesh/Admin/Expens/EmpSalary";
-import AdminDashboard from "./pages/adminDashboard";
+
 
 //import AdminDashboard from "./pages/adminDashboard";
 
