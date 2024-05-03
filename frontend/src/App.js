252b476e164809596f9337ef8326673106a6bd5a
KavishkaSashmitha <kavivlogs20@gmail.com>
2024-05-03 21:44:08 +0530

    last

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,14 @@
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
-} from "react-router-dom";
+} from 'react-router-dom';
 
-import Home from "./pages/Home";
-import { SidebarWithBurgerMenu } from "./components/navBar";
-
-
+import Home from './pages/Home';
+import { SidebarWithBurgerMenu } from './components/navBar';
 
 import Register from './pages/Register';
 import Dashboard from './pages/DashBoard';
@@ -18,85 +16,70 @@ import { EcommerceCard } from './pages/Products';
 
 import Cart from './pages/cart';
 
-
-
-
-
-
-
-import CreatPost from "./pages_Pasindu/CreateEmployee";
-import EditPost from "./pages_Pasindu/EditEmployee";
-import Posts from "./pages_Pasindu/Employee";
-import PostDetails from "./pages_Pasindu/EmployeeDetails";
-import "react-toastify/dist/ReactToastify.css";
+import CreatPost from './pages_Pasindu/CreateEmployee';
+import EditPost from './pages_Pasindu/EditEmployee';
+import Posts from './pages_Pasindu/Employee';
+import PostDetails from './pages_Pasindu/EmployeeDetails';
+import 'react-toastify/dist/ReactToastify.css';
 
 // import EmployeeChart from "./pages_Pasindu/Emp_Jobrole_Chart";
 // import MonthlySalChart from "./pages_Pasindu/Emp_Tot_SalChart";
 // import Emp_User_Chart from "./pages_Pasindu/Emp_User_Chart";
-import Display_Employee_Details from "./pages_Pasindu/Display_Employee_Details";
-import Employee_Dashboard from "./pages_Pasindu/Employee_Dashboard";
+import Display_Employee_Details from './pages_Pasindu/Display_Employee_Details';
+import Employee_Dashboard from './pages_Pasindu/Employee_Dashboard';
 // import HiestSalary from "./pages_Pasindu/HiestSalary";
-
 
 import CreateUser from './pages_kavindu/CreateUser';
 import UpdateUser from './pages_kavindu/UpdateUser';
 import User from './pages_kavindu/User';
 import AdminDashboard from './pages/adminDashboard';
 
-import SalaryReport from "./pages_Pasindu/SalaryReport";
-import DirectCartTable from "./pages/DirectOrder";
+import SalaryReport from './pages_Pasindu/SalaryReport';
+import DirectCartTable from './pages/DirectOrder';
 
 //Kumesh-----------------------
-import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 //Kumesh-user
 
-import Payment from "./pages-Kumesh/User/Payment";
-import CardDetails from "./pages-Kumesh/User/Cards";
-import AddCard from "./pages-Kumesh/User/AddCard";
-import UpdateCard from "./pages-Kumesh/User/UpdateCard";
-import PayPal from "./pages-Kumesh/User/PayPal";
-import PaypalCheckOut from "./pages-Kumesh/User/PaypalCheckOut";
-import PrintPage from "./pages-Kumesh/User/PrintPage";
+import Payment from './pages-Kumesh/User/Payment';
+import CardDetails from './pages-Kumesh/User/Cards';
+import AddCard from './pages-Kumesh/User/AddCard';
+import UpdateCard from './pages-Kumesh/User/UpdateCard';
+import PayPal from './pages-Kumesh/User/PayPal';
+import PaypalCheckOut from './pages-Kumesh/User/PaypalCheckOut';
+import PrintPage from './pages-Kumesh/User/PrintPage';
 
-import CashDelivery from "./pages-Kumesh/User/CashDelivery";
+import CashDelivery from './pages-Kumesh/User/CashDelivery';
 
 //Kumesh-Admin
-import NetIncome from "./pages-Kumesh/Admin/Income/Incomes";
-import Expens from "./pages-Kumesh/Admin/Expens/Expens";
+import NetIncome from './pages-Kumesh/Admin/Income/Incomes';
+import Expens from './pages-Kumesh/Admin/Expens/Expens';
 //Kumesh END-----------------------
 
-import EditOrder from "./pages_Ridmi/EditOrder";
-import Order from "./pages_Ridmi/Order";
-import "./index.css";
+import EditOrder from './pages_Ridmi/EditOrder';
+import Order from './pages_Ridmi/Order';
+import './index.css';
 
+import 'react-toastify/dist/ReactToastify.css';
 
-import "react-toastify/dist/ReactToastify.css";
+import SupplierReport from './pages_kavindu/SupplierReport';
+import MaterialReport from './pages_kavindu/MaterialCostReport';
 
-import SupplierReport from "./pages_kavindu/SupplierReport";
-import MaterialReport from "./pages_kavindu/MaterialCostReport";
+import Transport from './pages_Malshan/Transport';
+import Add_Driver from './pages_Malshan/Add_Driver';
+import Edit_Driver from './pages_Malshan/Edit_Driver';
+import Driver_Details from './pages_Malshan/Driver_Details';
+import FuelReport from './pages_Malshan/FuelReport';
+import { CartAdmin, CartDetails } from './pages/Cart-Admin';
+import CartChart from './pages/Cart-Chart';
+import OTPVerification from './pages/Manager-SignIn';
 
-import Transport from "./pages_Malshan/Transport";
-import Add_Driver from "./pages_Malshan/Add_Driver";
-import Edit_Driver from "./pages_Malshan/Edit_Driver";
-import Driver_Details from "./pages_Malshan/Driver_Details";
-import FuelReport from "./pages_Malshan/FuelReport";
-import { CartAdmin, CartDetails } from "./pages/Cart-Admin";
-import CartChart from "./pages/Cart-Chart";
-import OTPVerification from "./pages/Manager-SignIn";
+import { useAuth } from './middleware/authContext';
+import { CustomerLogin } from './pages/Customer-login';
 
-import { useAuth } from "./middleware/authContext";
-import { CustomerLogin } from "./pages/Customer-login";
+import './index.css';
 
-
-
-
-
-import "./index.css";
-
-import ImageUpload from "./imageUpload";
-
-
-
+import ImageUpload from './imageUpload';
 
 //Dileesha
 import AddProduct from './pages-Dileesha/AddProduct';
@@ -109,14 +92,10 @@ import ProductList from './pages/Direct_Order_Products';
 import CartAdminDashboard from './pages/Cart-AdminDashboard';
 import DirectOrdersTable from './pages/DirectOrders';
 
-
-
-
 import ProtectedRoute from './middleware/ProtectedRoute';
 import DeretOrders from './pages-Kumesh/Admin/Income/DeretOrders';
 import MaterialCost from './pages-Kumesh/Admin/Expens/MaterialCost';
 import EmpSalary from './pages-Kumesh/Admin/Expens/EmpSalary';
-
 
 function App() {
   const { isLoggedIn } = useAuth();
@@ -131,7 +110,6 @@ function App() {
         <Route path="/signUp" element={<Register />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        
 
         <Route path="/user/cart" element={<Cart />} />
         <Route path="/productCategory" element={<ProductList />} />
@@ -146,7 +124,6 @@ function App() {
 
         {/*Ridmi*/}
         <Route path="/my-orders" element={<MyOrders />} />
-
       </Route>
 
       <Route path="/" element={<Home />} />
@@ -222,7 +199,6 @@ function App() {
 
       <Route path="/otp" element={<OTPVerification />} />
 
-
       {/* Pasindu */}
       <Route path="/emp/add" element={<CreatPost />} />
       <Route path="/emp/edit/:id" element={<EditPost />} />
@@ -262,7 +238,6 @@ function App() {
       <Route path="/inventory/update/:id" element={<UpdateProduct />} />
       <Route path="/inventory" element={<InventoryList />} />
       <Route path="/inventory/report" element={<InventoryReport />} />
-
 
       <Route path="/test" element={<ImageUpload />} />
     </Routes>
